11613486fe9e6c4c81654adfb60d1516d2933060
Gytiss1 <juskysgytis@gmail.com>
2021-09-30 23:29:15 +0300

    API prijungimas ir semantic-ui pridejimas

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -32,6 +32,12 @@ namespace WebAPI
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            // Pridedame prieigą naršyklei gauti API rezultatus
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +53,9 @@ namespace WebAPI
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            // Pasiekiamumo politika pridedama prie API
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
