99f1408b63d3ca513ccf6ec2af6972ef02ccd2b6
Gytiss1 <juskysgytis@gmail.com>
2021-10-01 00:34:37 +0300

    Mediator pagrindiniame kontroleryje

--- WebAPI/Controllers/PagrindinisApiController.cs
+++ WebAPI/Controllers/PagrindinisApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace WebAPI.Controllers
 {
@@ -8,6 +10,12 @@ namespace WebAPI.Controllers
     [Produces("application/json")] // Priešpaskutinė Roberto paskaita (paskaitos vidurys)
     public class PagrindinisApiController : ControllerBase
     {
+        // Kiekvienas kontroleris turi turėti mediatorių, kad API susikalbėtų su aplikacija
+        private IMediator _mediator;
+
+        // ??= operatorius reiškia 'jeigu null, tai...'
+        // https://refactoring.guru/design-patterns/mediator/csharp/example
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
         
     }
 }
--- WebAPI/Controllers/RenginiaiController.cs
+++ WebAPI/Controllers/RenginiaiController.cs
@@ -3,30 +3,20 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Aplikacija.Renginiai;
 using Domenas;
-using Duomenys;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace WebAPI.Controllers
 {
     [ApiController]
     public class RenginiaiController : PagrindinisApiController
     {
-        private readonly IMediator _mediator;
-
-        // Įvedame Mediator iš aplikacijos į API
-        public RenginiaiController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         // Endpoint visų renginių grąžinimui naudojant Mediator
         // (visur naudojame async, kad nebūtų vykdomas kodas toliau kol nebaigta)
         [HttpGet]
         public async Task<ActionResult<List<Renginys>>> GetRenginiai()
-        {
-            return await _mediator.Send(new Sarasas.Query());
+        {   
+            // propsas gaunamas iš tėvinio kontrolerio
+            return await Mediator.Send(new Sarasas.Query());
         }
 
         // Endpoint skirtas vieno įrašo grąžinimui
