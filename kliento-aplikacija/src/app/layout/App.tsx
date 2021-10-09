21d99d47d6d5ddee0a6dff079a82895e2d08b97d
Gytiss1 <juskysgytis@gmail.com>
2021-10-09 09:38:32 +0300

    Renginio formos isvalymas pasirinkus "sukurti"

--- kliento-aplikacija/src/app/layout/App.tsx
+++ kliento-aplikacija/src/app/layout/App.tsx
@@ -4,12 +4,13 @@ import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import RenginiuLentele from '../../features/renginiai/dashboard/RenginiuLentele';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router';
+import { Route, useLocation } from 'react-router';
 import Pradinis from '../../features/home/HomePage';
 import RenginioForma from '../../features/renginiai/forma/RenginioForma';
 import RenginioDetales from '../../features/renginiai/detales/RenginioDetales';
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
@@ -18,7 +19,7 @@ function App() {
         <Route exact path='/' component={Pradinis} />
         <Route exact path='/renginiai' component={RenginiuLentele} />
         <Route path='/renginiai/:id' component={RenginioDetales} />
-        <Route path={['/sukurtiRengini', '/redaguoti/:id']} component={RenginioForma} />
+        <Route key={location.key} path={['/sukurtiRengini', '/redaguoti/:id']} component={RenginioForma} />
       </Container>
     </>
   );
